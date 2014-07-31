67a24475cf308bb8eee1a7f3bdefeb13d76ffa40
muraleekg <muraleekrishnagc@gmail.com>
2014-07-31 15:54:55 +0530

    finished static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,5 +38,9 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
+  config.include Capybara::DSL
   config.order = "random"
+  # Ruby on Rails Tutorial: sample application
+
+
 end
